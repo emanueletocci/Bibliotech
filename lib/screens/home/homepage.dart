f7d028256689b9c4cead3e940160df4ea24b8e73
Emanuele Tocci <emato03.felix@gmail.com>
2025-05-14 13:03:49 +0200

    Update homepage.dart

--- lib/screens/home/homepage.dart
+++ lib/screens/home/homepage.dart
@@ -12,12 +12,12 @@ class HomeScreen extends StatelessWidget {
           Stack(
             children: [
               Container(
-                height: 200,
+                height: 250,
                 decoration: BoxDecoration(
                   color: Colors.deepPurpleAccent,
                   borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(200),
-                    bottomLeft: Radius.circular(100),
+                    bottomRight: Radius.circular(70),
+                    bottomLeft: Radius.circular(150),
                   ),
                   boxShadow: [
                     BoxShadow(
@@ -29,22 +29,60 @@ class HomeScreen extends StatelessWidget {
                   ],
                 ),
               ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    left: 24,
-                    top: 80,
-                  ), // aggiungi padding per posizionare il testo
-                  child: Text(
-                    "Welcome!",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
+              Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 24, top: 80),
+                      child: Text(
+                        "Welcome!",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  SizedBox(height: 25,),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: TextButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.add),
+                      style: TextButton.styleFrom(
+                        minimumSize: Size(100, 70),
+                        backgroundColor: Colors.purpleAccent,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      label: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Aggiungi un libro!",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Text(
+                            "Stai leggendo qualcosa?",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
