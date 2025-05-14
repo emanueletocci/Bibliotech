4bc9f039dd07225567aa2c9a218d561cd825df51
emanueletocci <tocciemanuele03@gmail.com>
2025-05-14 17:20:38 +0200

    update palette

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/home/homepage.dart';
+import 'themes/themes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Bibliotech',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: appTheme,
       home: const HomeScreen(title: 'Homepage'),
     );
   }
---
+++ lib/themes/themes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.purple,
+    brightness: Brightness.light,
+  ),
+);
