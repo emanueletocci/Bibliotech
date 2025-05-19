1ac83a400d52e061401148fc08d30f659397f65e
Emanuele Tocci <83188429+emanueletocci@users.noreply.github.com>
2025-05-19 13:06:38 +0200

    Update aggiungi_libro.dart

--- lib/screens/aggiungi_libro/aggiungi_libro.dart
+++ lib/screens/aggiungi_libro/aggiungi_libro.dart
@@ -1,81 +1,129 @@
 import 'package:flutter/material.dart';
 
-class AggiungiLibro extends StatelessWidget {
+class AggiungiLibro extends StatefulWidget {
   final String immLibro;
 
-  AggiungiLibro({super.key, required this.immLibro});
+  const AggiungiLibro({super.key, required this.immLibro});
 
-  final TextEditingController titoloController = TextEditingController();
-  final TextEditingController autoriController = TextEditingController();
-  final TextEditingController linguaController = TextEditingController();
-  final TextEditingController isbnController = TextEditingController();
-  final TextEditingController dataPubblicazioneController = TextEditingController();
-  final TextEditingController noteController = TextEditingController();
+  @override
+  State<AggiungiLibro> createState() => _AggiungiLibroState();
+}
+
+class _AggiungiLibroState extends State<AggiungiLibro> {
+  final titolo = TextField();
+  final autori = TextField();
+  final lingua = TextField();
+  final isbn = TextField();
+  final dataPubblicazione = TextField();
+  final note = TextField();
 
   final List<String> categorie = ['Science', 'Thriller', 'Fantasy'];
   String? categoriaSelezionata;
 
   @override
   Widget build(BuildContext context) {
+    final String? immDaMostrare =
+        widget.immLibro.isEmpty ? null : widget.immLibro;
+
     return Scaffold(
-      appBar: AppBar(title: const Text("Aggiungi libro"), backgroundColor: Colors.deepPurple),
+      appBar: AppBar(
+        title: const Text("Aggiungi libro"),
+      ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(15),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Center(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.network(
-                "https://covers.openlibrary.org/b/isbn/9780385533225-L.jpg", 
-                  height: 200,
-                  width: 140,
-                  fit: BoxFit.cover,
-                ),
-              ),
+              child:
+                  widget.immLibro.isEmpty
+                      ? Container(
+                        height: 200,
+                        width: 160,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.deepPurple,
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: const Center(
+                          child: Icon(
+                            Icons.add,
+                            size: 40,
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                      )
+                      : ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.network(
+                          widget.immLibro,
+                          height: 200,
+                          width: 140,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) {
+                            return Container(
+                              height: 200,
+                              width: 140,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.deepPurple,
+                                  width: 2,
+                                ),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: const Center(
+                                child: Icon(
+                                  Icons.add,
+                                  size: 30,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
             ),
             const SizedBox(height: 20),
             TextField(
-              controller: titoloController,
               decoration: const InputDecoration(labelText: 'Titolo del libro'),
             ),
+
             const SizedBox(height: 10),
-            TextField(
-              controller: autoriController,
-              decoration: const InputDecoration(labelText: 'Autori'),
-            ),
+            TextField(decoration: const InputDecoration(labelText: 'Autori')),
+
             const SizedBox(height: 10),
-            TextField(
-              controller: linguaController,
-              decoration: const InputDecoration(labelText: 'Lingua'),
-            ),
+            TextField(decoration: const InputDecoration(labelText: 'Lingua')),
+
             const SizedBox(height: 10),
-            TextField(
-              controller: isbnController,
-              decoration: const InputDecoration(labelText: 'ISBN'),
-            ),
+            TextField(decoration: const InputDecoration(labelText: 'ISBN')),
+
             const SizedBox(height: 10),
             DropdownButtonFormField<String>(
               decoration: const InputDecoration(labelText: 'Categoria'),
-              items: categorie.map((cat) {
-                return DropdownMenuItem(value: cat, child: Text(cat));
-              }).toList(),
+              items:
+                  categorie.map((genere) {
+                    return DropdownMenuItem(value: genere, child: Text(genere));
+                  }).toList(),
               onChanged: (val) {
                 categoriaSelezionata = val;
               },
             ),
+
             const SizedBox(height: 10),
             TextField(
-              controller: dataPubblicazioneController,
-              decoration: const InputDecoration(labelText: 'Data Pubblicazione'),
+              decoration: const InputDecoration(
+                labelText: 'Data Pubblicazione',
+              ),
               keyboardType: TextInputType.datetime,
             ),
+
             const SizedBox(height: 10),
             TextField(
-              controller: noteController,
               decoration: const InputDecoration(labelText: 'Note personali'),
             ),
+
             const SizedBox(height: 20),
             Center(
               child: ElevatedButton.icon(
