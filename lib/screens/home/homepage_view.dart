ab8f79b5908a0119939a01f145168fede523f672
emanueletocci <tocciemanuele03@gmail.com>
2025-05-20 20:37:17 +0200

    Update homepage_view.dart

--- lib/screens/home/homepage_view.dart
+++ lib/screens/home/homepage_view.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 import '../../components/popup_aggiunta.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.title});
   final String title;
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(spacing: 30, children: <Widget>[Header(), Body()]),
+      child: Column(
+        spacing: 10,
+        children: <Widget>[
+          Header(),
+          Body(),
+        ],
+      ),
     );
   }
 }
