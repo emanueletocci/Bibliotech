6140b9631cedcc755781da049862d78ba787815c
ale <a.leo88@studenti.unisa.it>
2025-05-21 19:53:04 +0200

    aggiunta metodo per db

--- lib/models/db.dart
+++ lib/models/db.dart
@@ -33,15 +33,15 @@ Future<void> insertLibro(Libro libro) async {
   );
 }
 
-// A method that retrieves all the dogs from the dogs table.
+// A method that retrieves all the libri from the libri table.
 Future<List<Libro>> getLibri() async {
   // Get a reference to the database.
   final db = await libri;
 
-  // Query the table for all the dogs.
+  // Query the table for all the libri.
   final List<Map<String, Object?>> libriMap = await db.query('libri');
 
-  // Convert the list of each dog's fields into a list of `Dog` objects.
+  // Convert the list of each libro's fields into a list of `libro` objects.
   return [
     for (final libroMap in libriMap)
       Libro(
@@ -83,7 +83,7 @@ Future<void> deleteLibro(int id) async {
   // Get a reference to the database.
   final db = await libri;
 
-  // Remove the Dog from the database.
+  // Remove the libro from the database.
   await db.delete(
     'libri',
     // Use a `where` clause to delete a specific libro.
@@ -106,4 +106,58 @@ Future<void> updateLibro(Libro libro) async {
     // Pass the Libro's id as a whereArg to prevent SQL injection.
     whereArgs: [libro.id],
   );
+}
+
+Future<List<Libro>> getLibro(int id) async {
+  // Get a reference to the database.
+  final db = await libri;
+
+  // Query the table for all the libri.
+  final List<Map<String, Object?>> result = await db.query(
+    'libri',
+    where: 'id = ?',
+    whereArgs: [id],
+    limit: 1,
+  );
+
+  // Convert the list of each libro's fields into a list of `libro` objects.
+  if (result.isNotEmpty) {
+    final libroMap = result.first;
+    return [
+      Libro(
+        id: libroMap['id'] as int,
+        titolo: libroMap['titolo'] as String,
+        autori: libroMap['autori'] as String?,
+        numeroPagine: libroMap['numeroPagine'] as int?,
+        genere:
+            libroMap['genere'] != null
+                ? GenereLibro.values.firstWhere(
+                  (g) => g.toString() == 'GenereLibro.${libroMap['genere']}',
+                )
+                : null,
+        lingua: libroMap['lingua'] as String?,
+        trama: libroMap['trama'] as String?,
+        isbn: libroMap['isbn'] as String,
+        dataPubblicazione:
+            libroMap['dataPubblicazione'] != null
+                ? DateTime.tryParse(libroMap['dataPubblicazione'] as String)
+                : null,
+        voto:
+            libroMap['voto'] != null
+                ? (libroMap['voto'] as num).toDouble()
+                : null,
+        copertina: libroMap['copertina'] as String?,
+        note: libroMap['note'] as String?,
+        urlimg: libroMap['urlimg'] as String?,
+        stato:
+            libroMap['stato'] != null
+                ? StatoLibro.values.firstWhere(
+                  (s) => s.toString() == 'StatoLibro.${libroMap['stato']}',
+                )
+                : null,
+      ),
+    ];
+  } else {
+    return [];
+  }
 }
