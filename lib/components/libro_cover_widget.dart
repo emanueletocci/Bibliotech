899184089896ab75fdf20e93fcef0cdee856630e
emanueletocci <tocciemanuele03@gmail.com>
2025-05-21 16:13:02 +0200

    Update libro_cover_widget.dart

--- lib/components/libro_cover_widget.dart
+++ lib/components/libro_cover_widget.dart
@@ -1,55 +1,58 @@
-// lib/components/book_cover_display.dart
 import 'package:flutter/material.dart';
-import '../models/libro.dart'; // Assicurati che il percorso sia corretto
-import 'dart:io'; // Per l'uso di File
+import '../models/libro.dart';
+import 'dart:io';
+import '../screens/dettagli_libro/dettagli_libro_view.dart';
 
 class LibroCoverWidget extends StatelessWidget {
   final Libro libro;
-
   const LibroCoverWidget({super.key, required this.libro});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0), 
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(8.0),
-        child: _buildBookCover(libro),
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: GestureDetector(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8.0),
+          child: _buildBookCover(libro),
+        ),
+        onTap: () => {}, // Aprire pagina dettagli libro
       ),
     );
   }
-    Widget _buildBookCover(Libro libro) {
-      // Se la copertina è un URL di rete
-      if (libro.copertina!.startsWith('http://') ||
-          libro.copertina!.startsWith('https://')) {
-        return Image.network(
-          libro.copertina!,
-          fit: BoxFit.cover,
-          errorBuilder:
-              (context, error, stackTrace) => const Center(
-                child: Icon(Icons.broken_image, size: 50, color: Colors.red),
-              ),
-        );
-      } else if(libro.copertina == 'assets/images/book_placeholder.jpg') {
-        // Se la copertina è il placeholder di default
-        return Image.asset(
-          libro.copertina!,
-          fit: BoxFit.cover,
-          errorBuilder:
-              (context, error, stackTrace) => const Center(
-                child: Icon(Icons.broken_image, size: 50, color: Colors.red),
-              ),
-        );
-      } else {
-        // Altrimenti mostro il file locale
-        return Image.file(
-          File(libro.copertina!),
-          fit: BoxFit.cover,
-          errorBuilder:
-              (context, error, stackTrace) => const Center(
-                child: Icon(Icons.broken_image, size: 50, color: Colors.red),
-              ),
-        );
-      }
+
+  Widget _buildBookCover(Libro libro) {
+    // Se la copertina è un URL di rete
+    if (libro.copertina!.startsWith('http://') ||
+        libro.copertina!.startsWith('https://')) {
+      return Image.network(
+        libro.copertina!,
+        fit: BoxFit.cover,
+        errorBuilder:
+            (context, error, stackTrace) => const Center(
+              child: Icon(Icons.broken_image, size: 50, color: Colors.red),
+            ),
+      );
+    } else if (libro.copertina == 'assets/images/book_placeholder.jpg') {
+      // Se la copertina è il placeholder di default
+      return Image.asset(
+        libro.copertina!,
+        fit: BoxFit.cover,
+        errorBuilder:
+            (context, error, stackTrace) => const Center(
+              child: Icon(Icons.broken_image, size: 50, color: Colors.red),
+            ),
+      );
+    } else {
+      // Altrimenti mostro il file locale
+      return Image.file(
+        File(libro.copertina!),
+        fit: BoxFit.cover,
+        errorBuilder:
+            (context, error, stackTrace) => const Center(
+              child: Icon(Icons.broken_image, size: 50, color: Colors.red),
+            ),
+      );
     }
+  }
 }
