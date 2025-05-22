001a756cfc3f1d0ee6204a488eee17ae98b07474
emanueletocci <tocciemanuele03@gmail.com>
2025-05-22 13:25:26 +0200

    implementazione libreria da branch secondario

--- lib/screens/libreria/libreria_view.dart
+++ lib/screens/libreria/libreria_view.dart
@@ -1,71 +1,137 @@
-/*
- * Questo file contiene l'implemenazione della struttura della schermata principale dell'applicazione, contenente lo Scaffold e la barra di navigazione in basso. Le altre schermate (homepage, libreria, statistiche) sono 
- * implementate in file separati e richiamate qui, al fine di mantenere il codice piú ordinato ed evitare di innestare scaffold.
-*/
-
 import 'package:flutter/material.dart';
-import 'home/homepage_view.dart';
-import 'libreria/libreria_view.dart';
-import '../components/popup_aggiunta.dart';
+import 'package:sqflite/sqflite.dart';
 
-class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
-
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
-
-  final List<Widget> _pages = [
-    HomeScreen(title: "Homepage"),
-    Libreria(),
-  ];
-
-  void _onTabTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
+class Libreria extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: _selectedIndex,  // mostro solo la schermata corrispondente a questo indice
-        children: _pages,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onTabTapped, // il parametro index (tab corrente) viene passato automaticamente,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SearchBarCustom(),
+              Generi(),
+              Expanded(
+                child: GridView.count(
+                  padding: EdgeInsets.all(7),
+                  crossAxisCount: 2,
+                  children: [
+                    Libro(path: 'assets/images/book2.jpg'),
+                    Libro(path: 'assets/images/book3.jpg'),
+                    Libro(path: 'assets/images/book4.jpg'),
+                    Libro(path: 'assets/images/book5.jpg'),
+                    Libro(path: 'assets/images/book6.jpg'),
+                    Libro(path: 'assets/images/book1.jpg'),
+                  ],
+                ),
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.book),
-            label: 'Libreria',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bar_chart),
-            label: 'Statistiche',
-          ),
-        ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            isScrollControlled: true,  // consente al popup di occupare tutto lo schermo
-            builder: (context) {
-              return const PopupAggiunta();
-            },
-          );
-        },
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class SearchBarCustom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(21)),
+        hintText: 'Search Book',
+        prefixIcon: Icon(Icons.search),
       ),
+    );
+  }
+}
+
+class Libro extends StatelessWidget {
+  final String path;
+  const Libro({Key? key, required this.path}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => {print("prova in debug console")},
+      child: Container(
+        width: 40,
+        height: 40,
+        margin: EdgeInsets.all(4),
+        child: Image(image: AssetImage(path)),
+      ),
+    );
+  }
+}
+
+class Generi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.all(8.0)),
+              Image(
+                image: AssetImage('assets/images/cover1.jpg'),
+                width: 60,
+                height: 90,
+                fit: BoxFit.cover,
+              ),
+              Text('Fantasy', style: TextStyle(fontWeight: FontWeight.bold)),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.all(8.0)),
+              Image(
+                image: AssetImage('assets/images/cover2.jpg'),
+                width: 60,
+                height: 90,
+                fit: BoxFit.cover,
+              ),
+              Text('Thriller', style: TextStyle(fontWeight: FontWeight.bold)),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.all(8.0)),
+              Image(
+                image: AssetImage('assets/images/cover3.jpg'),
+                width: 60,
+                height: 90,
+                fit: BoxFit.cover,
+              ),
+              Text('Leteraly', style: TextStyle(fontWeight: FontWeight.bold)),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 15.0, right: 15.0, bottom: 20.0),
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.all(8.0)),
+              Image(
+                image: AssetImage('assets/images/cover4.jpg'),
+                width: 60,
+                height: 90,
+                fit: BoxFit.cover,
+              ),
+              Text('Science', style: TextStyle(fontWeight: FontWeight.bold)),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
