9a5b001105445334e99a5e1f06946c8741ef2b41
emanueletocci <tocciemanuele03@gmail.com>
2025-05-27 20:07:52 +0200

    implementazione db su desktop

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
+import 'dart:io';
 import 'package:bibliotech/screens/main_view.dart';
 import 'package:flutter/material.dart';
 import 'models/libreria.dart';
 import 'themes/themes.dart';
 import 'package:provider/provider.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main() async {
+  // Assicuro che il framwork Flutter sia inizializzato prima di eseguire interazioni con il SO o plugin nativi
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Il plugin sqflite non funziona su desktop, occorre utilizzare sqflite_ffi
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
+
   final libreria = Libreria();
-  
   // Inizializz la libreria caricando i libri dal database
   await libreria.init(); 
 
